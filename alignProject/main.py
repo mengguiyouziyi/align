7f16b517a076f411aaeb3bad2dca10fe5febb7ed
mengguiyouziyi <775618369@qq.com>
2019-04-28 10:27:24 +0800

    one

--- alignProject/main.py
+++ alignProject/main.py
@@ -1,6 +1,6 @@
-from .alignCTK import alignCTK
-from .breakSenStrict import breakEnSen, breakZhSen
 from .extractWordDocx2txt import doc2txt_docx2txt
+from .breakSenStrict import breakEnSen, breakZhSen
+from .alignCTK import alignCTK
 from .lineWriteExcel import toExcel
 
 
