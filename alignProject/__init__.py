7e9ea499ff33aaf8b1dd3d02ebfba6c7d9790f26
mengguiyouziyi <775618369@qq.com>
2019-05-07 14:43:46 +0800

    one

---
+++ alignProject/__init__.py
@@ -0,0 +1,5 @@
+from .extractWordDocx2txt import docx2text
+from .breakEnSenStrict import breakEnSen
+from .breakZhSenStrict import breakZhSen
+from .alignCTK import alignCTK
+from .lineWriteExcel import toExcel
