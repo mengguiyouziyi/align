47e6b5b62275fc07158e8a4b76813d66bc0a4327
mengguiyouziyi <775618369@qq.com>
2019-05-08 09:18:51 +0800

    one

--- alignProject/main.py
+++ alignProject/main.py
@@ -78,18 +78,30 @@ if __name__ == '__main__':
             # 从doc到txt
             en_org_path = en_doc_name + '-org.en'
             zh_org_path = zh_doc_name + '-org.zh'
+            if not os.path.exists(en_org_path):
+                os.makedirs(en_org_path)
+            if not os.path.exists(zh_org_path):
+                os.makedirs(zh_org_path)
             docx2text(en_doc_path, en_org_path)
             docx2text(zh_doc_path, zh_org_path)
             # 从txt到breakSen
             en_sen_path = en_doc_name + '-sen.en'
             zh_sen_path = zh_doc_name + '-sen.zh'
+            if not os.path.exists(en_sen_path):
+                os.makedirs(en_sen_path)
+            if not os.path.exists(zh_sen_path):
+                os.makedirs(zh_sen_path)
             breakEnSen(en_org_path, en_sen_path)
             breakZhSen(zh_org_path, zh_sen_path)
             # 从sen到align
             align_label_path = en_doc_name + '-align-label.txt'
+            if not os.path.exists(align_label_path):
+                os.makedirs(align_label_path)
             alignCTK(en_sen_path, zh_sen_path, align_label_path)
             # 从align-label到excel
             excel_path = en_doc_name + '.xls'
+            if not os.path.exists(excel_path):
+                os.makedirs(excel_path)
             toExcel(align_label_path, en_sen_path, zh_sen_path, excel_path)
 
         time.sleep(1)
