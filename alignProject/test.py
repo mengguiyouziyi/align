95ea4f32777c790ff371f377314534539da47aed
mengguiyouziyi <775618369@qq.com>
2019-06-05 15:54:23 +0800

    one

--- alignProject/test.py
+++ alignProject/test.py
@@ -8,35 +8,42 @@ from lineWriteExcel import toExcel
 
 
 def addDir(file, bdir):
+    if not os.path.exists(bdir):
+        os.makedirs(bdir)
     return os.path.join(bdir, file)
 
 
-bdir = '/home/wande/文档/docx'
-en_file = 'IB BAY 1101042_V4.0.docx'
-cn_file = 'IB BAY 1101042_V4.0-CN.docx'
-en_doc_name = os.path.splitext(en_file)[0]
-zh_doc_name = os.path.splitext(en_file)[0]
-en_file = addDir(en_file, bdir)
-cn_file = addDir(cn_file, bdir)
-en_doc_name = addDir(en_doc_name, bdir)
-zh_doc_name = addDir(zh_doc_name, bdir)
+def subIllegal(file):
+    r = re.compile(r'[^-\u4e00-\u9fa5A-Za-z0-9_/]')
+    all = os.path.splitext(file)
+    return r.sub(r'-', all[0])
 
-r = re.compile(r'[^-\u4e00-\u9fa5A-Za-z0-9_/]')
-en_doc_path = r.sub('-', en_file.replace('.docx', '')) + '.docx'
-zh_doc_path = r.sub('-', cn_file.replace('.docx', '')) + '.docx'
+
+sdir = '/home/wande/文档/docx/source'
+mdir = '/home/wande/文档/docx/middle'
+rdir = '/home/wande/文档/docx/result'
+en_doc = 'IB BAY 1101042_V4.0.docx'  # 文件名称（带后缀）
+cn_doc = 'IB BAY 1101042_V4.0-CN.docx'
+en_name = subIllegal(en_doc)
+cn_name = subIllegal(cn_doc)
+en_org = en_name + '-org.en'
+cn_org = cn_name + '-org.cn'
+en_sen = en_name + '-sen.en'
+cn_sen = cn_name + '-sen.cn'
+
 # 从doc到txt
-en_org_path = en_doc_name + '-org.en'
-zh_org_path = zh_doc_name + '-org.zh'
-docx2text(en_file, en_org_path)
-docx2text(cn_file, zh_org_path)
+en_org_path = addDir(en_org, mdir)
+cn_org_path = addDir(cn_org, mdir)
+docx2text(addDir(en_doc, sdir), en_org_path)
+docx2text(addDir(cn_doc, sdir), cn_org_path)
 # 从txt到breakSen
-en_sen_path = en_doc_name + '-sen.en'
-zh_sen_path = zh_doc_name + '-sen.zh'
+en_sen_path = addDir(en_sen, mdir)
+cn_sen_path = addDir(cn_sen, mdir)
 breakEnSen(en_org_path, en_sen_path)
-breakZhSen(zh_org_path, zh_sen_path)
+breakZhSen(cn_org_path, cn_sen_path)
 # 从sen到align
-align_label_path = en_doc_name + '-align-label.txt'
-alignCTK(en_sen_path, zh_sen_path, align_label_path)
+align_label_path = addDir(en_name + '-align-label.txt', mdir)
+alignCTK(en_sen_path, cn_sen_path, align_label_path)
 # 从align-label到excel
-excel_path = en_doc_name + '.xls'
-toExcel(align_label_path, en_sen_path, zh_sen_path, excel_path)
+excel_path = addDir(en_name + '.xls', rdir)
+toExcel(align_label_path, en_sen_path, cn_sen_path, excel_path)
